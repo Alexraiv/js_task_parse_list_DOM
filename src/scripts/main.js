66f7354ddbfb7496377d9563d6714b0498badecd
Oleksandr Voronkin <94705289+Alexraiv@users.noreply.github.com>
2025-05-16 22:47:36 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,48 @@
 'use strict';
 
 // write code here
+const PeopleList = document.querySelector('ul');
+
+const sortList = (list) => {
+  const soretedList = list.sort((a, b) => {
+    const salaryA = Number(a.salary.replace(/\$|,/g, ''));
+    const salaryB = Number(b.salary.replace(/\$|,/g, ''));
+
+    return salaryB - salaryA;
+  });
+
+  PeopleList.innerHTML = '';
+
+  for (let i = 0; i < soretedList.length; i++) {
+    const li = document.createElement('li');
+
+    li.textContent = `${soretedList[i].name}`;
+    li.dataset.position = `${soretedList[i].position}`;
+    li.dataset.salary = `${soretedList[i].salary}`;
+    li.dataset.age = `${soretedList[i].age}`;
+
+    PeopleList.append(li);
+  }
+
+  return PeopleList;
+};
+const getEmployees = (list) => {
+  const employees = [];
+
+  for (let i = 0; i < list.children.length; i++) {
+    const employee = {
+      name: list.children[i].textContent.trim(),
+      position: list.children[i].dataset.position,
+      salary: list.children[i].dataset.salary,
+      age: list.children[i].dataset.age,
+    };
+
+    employees.push(employee);
+  }
+
+  return employees;
+};
+
+const employeesList = getEmployees(PeopleList);
+
+sortList(employeesList);
